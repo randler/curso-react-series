ad62c9d1e09ab4806065b8c1dde2fce0dcfc7446
Randler Ferraz <randlersi@gmail.com>
2018-08-22 16:50:28 -0300

    Finalizando layout de series page

--- src/pages/SeriesPage.js
+++ src/pages/SeriesPage.js
@@ -12,14 +12,23 @@ const SeriesPage = props => (
             renderItem={({item, index }) => (
             <SerieCard 
                 serie={item}
-                isFirstColumn={index%2==0} />
+                isFirstColumn={index%2===0} />
         )}
         keyExtractor={item => item.id} 
         numColumns={2}
+        ListHeaderComponent={props => (<View style={styles.header} />)}
+        ListHeaderComponent={props => (<View style={styles.footer} />)}
         /> 
     </View>    
 )
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    header: {
+        marginTop: 5,
+    },
+    footer: {
+        marginBottom: 5,
+    }
+});
 
 export default SeriesPage;
