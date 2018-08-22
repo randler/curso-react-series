859536ff464d7af7d7bd491aaecb4827b475dd9c
Randler Ferraz <randlersi@gmail.com>
2018-08-22 16:02:12 -0300

    finalizando o estilo do card

--- src/components/SerieCard.js
+++ src/components/SerieCard.js
@@ -1,19 +1,26 @@
 //import liraries
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { 
+    View, 
+    Text, 
+    StyleSheet, 
+    Image, 
+    Dimensions
+} from 'react-native';
 
 // create a component
 const SerieCard = ({ serie }) => {
     return (
         <View style={styles.container}>
-            <View style={styles.cell} >
-                <View style={styles.line} >
-                    <Image
-                        style={styles.imageItem} 
-                        source={{uri: serie.img}} />
-                    <Text>{`${serie.id} - ${serie.title}`}</Text>
+            <View style={styles.line} >
+                <Image
+                    style={styles.imageItem} 
+                    source={{uri: serie.img}}
+                    resizeMode="cover" />
+                <View style={styles.cardTitleWrapper}>
+                    <Text style={styles.cardTitle}>{`${serie.id} - ${serie.title}`}</Text>
                 </View>
-            </View> 
+            </View>
         </View>
     );
 };
@@ -22,30 +29,39 @@ const SerieCard = ({ serie }) => {
 const styles = StyleSheet.create({
     container: {
         padding: 5,
-        flex: 1
-    },
-    cell: {
-        flexDirection: 'row',
+        flex: 1,
+        height: Dimensions.get('window').width / 2,
     },
     line: {
-
         marginHorizontal: 5,
         marginBottom: 5,
         backgroundColor: '#FFF',
         borderRadius: 5,
-        padding: 5,
         elevation: 1,
-        height: 150,
         flex: 1
     },
     imageItem: {
         flex: 4,
         borderTopLeftRadius: 5,
         borderTopRightRadius: 5,
-        marginBottom: 5,
+        marginBottom: 50,
     },
-    textItem: {
+    cardTitleWrapper: {
+        backgroundColor: '#FFFFFF',
+        height: 50,
+        paddingLeft: 5,
+        paddingTop: 5,
+        borderBottomLeftRadius: 5,
+        borderBottomRightRadius: 5,
+
+        position: 'absolute',
+        bottom: 0,
+        opacity: .8,
+        width: '100%'
+    },
+    cardTitle: {
         flex: 2,
+        fontSize: 15,
         fontWeight: 'bold',
     }
 });
